954a4ef486f22d0fec777e9783e2a7f3b35081d0
Zeefakiller <145507603+Zeefakiller@users.noreply.github.com>
2023-09-26 11:10:15 +0000

    conversation

--- 01_basic/01_conversation.js
+++ 01_basic/01_conversation.js
@@ -1,9 +1,21 @@
 let score = "Zaki";
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof (valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+let isLogedIn = 1;
+
+let booleanIsLoggedIn =  Boolean(isLogedIn);
+// console.log(booleanIsLoggedIn);
+
+
+ let someNumber = 11; 
+
+ let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
